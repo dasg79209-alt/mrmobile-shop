b8abcf8586844a2ab0b2985db3034b4d28e09db7
dasg79209-alt <dasg79209@gmail.com>
2026-09-24 19:08:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ ADMIN_PASSWORD = "admin"
 SHOP_UPI_ID = "8927294076@naviaxis"  # আপনার আসল UPI ID এখানে বসাবেন
 
 # ডাইনামিক স্লাইডার ব্যানার (যার মধ্যে শোরুম অফারও ঘুরবে)
-banners = 
+banners = [
     {
         "id": 1,
         "title": "Mister Mobile Now at Gidhni, Jhargram, Silda",
